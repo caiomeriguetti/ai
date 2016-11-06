e27f809129c7dffbaecb55f6e706643ab6e7ee7f
Caio Meriguetti <caiomeriguetti@gmail.com>
2016-11-06 15:54:19 -0200

    branch and bound algorithm

---
+++ app.js
@@ -0,0 +1,109 @@
+function create_inputs(n) {
+	var namesElement = $("<ul></ul>");
+
+	$("#origin").append("<option >Origin</option>");
+	$("#dest").append("<option >Destination</option>");
+
+	for (var i = 1; i <= n; i++) {
+		namesElement.append($('<li>'+(i)+' = <input type="text" data-num="'+(i)+'"></input></li>'))
+		$("#origin").append("<option value='"+i+"' data-namefor='"+i+"'>"+i+"</option>");
+		$("#dest").append("<option value='"+i+"' data-namefor='"+i+"'>"+i+"</option>");
+	}
+
+	$('.names').children().remove();
+	$('.names').append(namesElement);
+
+	var table = $("<table><thead></thead><tbody></tbody></table>");
+	var thead = $("<tr></tr>");
+	table.find('thead').append(thead);
+	thead.append("<th></th>");
+	for (var i = 1; i <= n; i++) {
+		thead.append("<th data-namefor='"+i+"'>"+i+"</th>");
+	}
+	 
+	for (var i = 1; i <= n; i++) {
+		var line = $("<tr></tr>");
+		line.append("<td data-namefor='"+i+"'>"+i+"</td>");
+		for (var j = 1; j <= n; j++) {
+			var col = $("<td></td>");
+			var input = $("<input class='edge-input' type='text' data-edge='"+i+"-"+j+"'/>");
+			if (i==j) {
+				input.prop("readonly", true);
+				input.prop("disabled", true);
+			}
+			col.append(input);
+			line.append(col);
+		}
+		
+		table.find("tbody").append(line);
+	}
+
+	$(".adj-matrix").html(table);
+
+}
+
+var graph = new Graph({});
+function calculate_neighbors () {
+	$("[data-edge]").each(function (index, item) {
+		
+		var edge = $(item).data('edge').split('-');
+		var val = $(item).val();
+
+		if (val != "") {
+			graph.addEdge(new Vertex({}, edge[0]), new Vertex({}, edge[1]), val);
+			graph.addEdge(new Vertex({}, edge[1]), new Vertex({}, edge[0]), val);
+		}
+
+	});
+}
+
+function find_path (orig, dest) {
+	calculate_neighbors();
+	var bb = new BranchAndBound({});
+
+	var path = bb.pathFind(graph, orig, dest);
+
+	console.log(path.toString());
+}
+
+function test_data() {
+	create_inputs(8);
+	$('[data-num="1"]').val("A");
+	$('[data-num="2"]').val("B");
+	$('[data-num="3"]').val("C");
+	$('[data-num="4"]').val("D");
+	$('[data-num="5"]').val("E");
+	$('[data-num="6"]').val("F");
+	$('[data-num="7"]').val("G");
+	$('[data-num="8"]').val("S");
+	$('[data-num]').trigger('keyup');
+
+	$("[data-edge='8-1'],[data-edge='1-8']").val(3);
+	$("[data-edge='8-4'],[data-edge='4-8']").val(4);
+	$("[data-edge='1-4'],[data-edge='4-1']").val(5);
+	$("[data-edge='1-2'],[data-edge='2-1']").val(4);
+	$("[data-edge='2-5'],[data-edge='5-2']").val(5);
+	$("[data-edge='4-5'],[data-edge='5-4']").val(2);
+	$("[data-edge='2-3'],[data-edge='3-2']").val(4);
+	$("[data-edge='6-5'],[data-edge='5-6']").val(4);
+	$("[data-edge='6-7'],[data-edge='7-6']").val(3);
+
+}
+
+$(function () {
+
+	$("#create-inputs").click(function () {
+		create_inputs($("#num-vertices").val());
+	});
+
+	$(document).on("keyup", "[data-num]", function (){
+		var num = $(this).data("num");
+		var name = $(this).val();
+		if (name != "") {
+			$('body [data-namefor="'+num+'"]').html(name);
+		}
+	});
+
+	
+
+});
---
+++ lib.js
@@ -0,0 +1,196 @@
+function Path(self) {
+	var me = this;
+
+	me.vertices = [];
+	me.cost = null;
+
+	self.addVertex = function (v) {
+		me.vertices.push(v);
+	};
+
+	self.getVertices = function () {
+		return me.vertices;
+	}
+
+	self.setVertices = function (vertices) {
+		me.vertices = vertices;
+	};
+
+	self.setCost = function (cost) {
+		me.cost = cost;
+	};
+
+	self.getCost = function () {
+		return me.cost;
+	};
+
+	self.getLastVertex = function () {
+		return me.vertices[me.vertices.length - 1];
+	};
+
+	self.clone = function () {
+		var newPath = new Path({});
+		for (var i = 0; i < me.vertices.length; i++) {
+			newPath.addVertex(me.vertices[i]);
+		}
+		newPath.setCost(me.cost);
+
+		return newPath;
+	};
+
+	self.isGoingBack = function () {
+		var check = {};
+		for (var i = 0; i < me.vertices.length; i++) {
+			if (check[me.vertices[i].getId()] === true) return true;
+			
+			check[me.vertices[i].getId()] = true;
+		}
+		return false;
+	}
+
+	self.toString = function () {
+		var ids = []
+		for (var i = 0; i < me.vertices.length; i++) {
+			ids.push(me.vertices[i].getId());
+		}
+		return "[ " + ids.toString() + " = " + me.cost + " ]";
+	};
+
+	return self;
+}
+
+function Vertex(self, id, name) {
+
+	var me = this;
+
+	me.name = name;
+	me.id = id;
+
+	self.setName = function (name) {
+		me.name = name;
+	};
+
+	self.getName = function () {
+		return me.name;
+	};
+
+	self.setId = function (id) {
+		me.id = id;
+	};
+
+	self.getId = function () {
+		return me.id;
+	};
+
+	self.toString = function () {
+		return me.id;
+	};
+
+	return self;
+}
+
+function Edge (self, v1, v2, value) {
+	var me = this;
+	me.v1 = v1;
+	me.v2 = v2;
+	me.value = value;
+
+	self.getV1 = function () {
+		return me.v1;
+	};
+
+	self.getV2 = function () {
+		return me.v2;
+	};
+
+	self.getValue = function () {
+		return me.value;
+	};
+
+	return self;
+}
+
+function Graph (self) {
+	var me = this;
+	me.edges = {};
+	me.edgesCheck = {};
+
+	self.addEdge = function (v1, v2, value) {
+
+		if (me.edges[v1] == undefined) me.edges[v1] = [];
+		var edgeId = v1.getId() + "-" + v2.getId();
+		if (me.edgesCheck[edgeId] === true) {
+			return false;
+		}
+
+		me.edgesCheck[edgeId] = true;
+		
+		var e = new Edge({}, v1, v2, value);
+		me.edges[v1].push(e);
+
+
+		return e;
+	};
+
+	self.getNeighbors = function (v) {
+		return me.edges[v];
+	};
+
+	return self;
+	
+}
+
+function BranchAndBound (self) {
+	var me = this;
+	self.pathFind = function (graph, v1, v2) {
+
+		var path_queue = [];
+		var start_path = new Path({});
+		start_path.addVertex(new Vertex({}, v1));
+
+		path_queue.splice(0, 0, start_path);
+		var  k = 0;
+		var visiteds = {};
+
+		while (path_queue.length > 0) {
+			var removedPath = path_queue.splice(0, 1)[0];
+			var lastVertex = removedPath.getLastVertex();
+
+			if (lastVertex.getId() == v2) {
+				return removedPath;
+			}
+
+			if (visiteds[lastVertex.getId()] === true) {
+				continue;
+			}
+
+			visiteds[lastVertex.getId()] = true;
+
+			currentNeighbors = graph.getNeighbors(lastVertex.getId());
+
+			for (var i = 0; i < currentNeighbors.length; i++) {
+				var neighborData = currentNeighbors[i];
+
+				var path = removedPath.clone();
+				path.addVertex(new Vertex({}, neighborData.getV2()));
+				path.setCost(removedPath.getCost() + parseFloat(neighborData.getValue()));
+
+				if (path.isGoingBack()) continue;
+
+				if (path_queue[0] && path_queue[0].getCost() > path.getCost()) {
+					path_queue.splice(0, 0, path);
+				} else {
+					path_queue.push(path);
+				}
+
+			}
+
+			console.log(path_queue.toString());
+			
+		}
+		return null;
+	};
+
+	return self;
+
+}
